5458f8d15972f525c1d33de42ec0a937e3b55b59
avaxzhang <zx_genius@126.com>
2022-07-18 18:01:20 +0800

    add
     入门

---
+++ Tensorflow 入门.md
@@ -0,0 +1,25 @@
+Tensorflow 学习资料
+
+# 教程资源
+
+1   官方教程 https://developers.google.cn/machine-learning/crash-course
+2   莫烦的机器学习 https://space.bilibili.com/243821484#/  https://morvanzhou.github.io/
+3   吴恩达的机器学习 https://www.bilibili.com/video/av9912938/?p=1
+4   斯坦福2018季CS231n（李飞飞）https://www.bilibili.com/video/av17204303 https://www.bilibili.com/video/av9156347/?from=search&seid=6905181275544516403
+5   李宏毅的机器学习 https://www.bilibili.com/video/av17204303  ttps://www.bilibili.com/video/av9770302/ https://www.bilibili.com/video/av16543434/
+https://www.bilibili.com/video/av16543434/ （一天系列）
+6   深度学习 TensorFlow https://www.bilibili.com/video/av21099556
+
+
+
+# Github 教学资源
+
+
+1   Torfi的TensorFlow教程 https://github.com/instillai/TensorFlow-Course#why-use-tensorflow
+
+2   TensorFlow-Examples https://github.com/aymericdamien/TensorFlow-Examples
+3   TensorFlow courses  https://github.com/instillai/TensorFlow-Course
+4   TensorFlow Tutorials https://github.com/Hvass-Labs/TensorFlow-Tutorials
+5   standford 教程        https://github.com/chiphuyen/stanford-tensorflow-tutorials
+6   TensorFlow 中文教程   https://github.com/czy36mengfei/tensorflow2_tutorials_chinese
+7   pytorch 资源    https://pytorch.org/tutorials/ 
---
+++ cross_entroy_python.py
@@ -0,0 +1,44 @@
+
+'''
+运行环境：https://idex.oa.com/jupyter/instances/cc14623c81e15ccc/user/avaxzhang/lab/tree/pytorch/%E4%BA%A4%E5%8F%89%E7%86%B5.ipynb
+
+@author: zhangxian
+@brief Description : 交叉熵的python实现 ,X 2-d array , Y 1-d array
+
+'''
+import tensorflow as    tf
+import numpy as np
+import torch as torch
+
+def cross_entropy_with_numpy(prob_y, y):    
+   return -np.mean(np.sum(y * np.log(prob_y), axis = 1))
+
+def cross_entropy_with_tensorflow(prob_y, y): #未测试
+    return -tf.reduce_mean(tf.reduce_sum(y * tf.log(prob_y), axis = 1))
+
+def cross_entropy_with_torch(prob_y, y):    #未测试 
+    #x_np = torch.from_numpy(np_array) # ndarry -> tensor
+    prob = torch.from_numpy(prob_y)
+    gr = torch.from_numpy(y)
+    return -torch.mean(torch.sum(gr * torch.log(prob), axis = 1))
+
+def softmax_with_numpy(x):
+    return np.exp(x)/np.sum(np.exp(x))  #分母这里是全部求和
+
+def one_hot(x,cls):
+    tmp = np.zeros((x.shape[0],cls))
+    for i,k in enumerate(x):
+        tmp[i][k] =1
+
+def cross_entropy(X,Y):
+    prob_y = softmax_with_numpy(X) 
+    onehotY = one_hot(Y,2)
+    return cross_entropy_with_numpy(prob_y, onehotY)
+
+
+if __name__ == "__main__"::
+
+    N  = 100000
+    X = np.random.random([N,2])
+    Y = (np.random.random([N,1])>0.5).astype(int)
+    print(cross_entropy(X,Y))
